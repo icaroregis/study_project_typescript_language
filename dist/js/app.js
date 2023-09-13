c46b3c9924b41476d115dc2d934fb45560187004
icaroalmeida-somma <icaro.almeida@somma-it.com>
2023-09-13 20:25:03 -0300

    Início do projeto

---
+++ dist/js/app.js
@@ -0,0 +1,6 @@
+// Regras de negócio.
+// Uma negociação não pode ser modificada depois de criada
+// Uma negociação obrigatoriamente tem uma data, uma quantidade e um valor
+import Negociacao from './models/negociacao.js';
+const negociacao = new Negociacao(new Date(), 10, 100);
+console.log(negociacao.volume);
