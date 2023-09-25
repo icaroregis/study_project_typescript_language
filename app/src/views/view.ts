4c24bc227ed8dadcfe1cc611b960650e5ef41c0f
icaroalmeida-somma <icaro.almeida@somma-it.com>
2023-09-24 23:28:05 -0300

    Criação de decorators com parametros e sem parametros

--- app/src/views/view.ts
+++ app/src/views/view.ts
@@ -1,6 +1,7 @@
 //Só View tem acesso a um propriedade privada e seus filhos no caso a quem nós extendermos. Nenhuma outra classe terá acesso a não ser as classes filhas.
 
 //A classe abstrata não pode ser criada uma instancia diretamente dela, só pode se o filho herda essa classe e se o filho for instanciado.
+import { inspect } from '../decorators/inspect.js';
 import { logarTempoDeExecucao } from '../decorators/logarTempoDeExecucao.js';
 
 export abstract class View<T> {
@@ -10,7 +11,10 @@ export abstract class View<T> {
     protected elemento: HTMLElement = document.querySelector(seletor) as HTMLElement,
   ) {}
 
+  //com parametros
   @logarTempoDeExecucao(true)
+  //sem parametros
+  @inspect
   public update(model: T): void {
     this.elemento.innerHTML = this.template(model);
   }
